ff0725566ff19fcae494c75a19c0e22d6084de70
Rumman Ahmad <rummanahmad05@gmail.com>
2023-05-06 18:08:45 +0530

    final

--- premium.py
+++ premium.py
@@ -4,16 +4,12 @@ import numpy as np
 import pickle
 import json
 from flask_cors import CORS
-import sklearn
-from sklearn.preprocessing import OneHotEncoder
-
 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
+import urllib.parse
 pd.set_option('display.max_columns', None)
-
 
 
 app = Flask(__name__)
@@ -36,80 +32,88 @@ def test():
 
 
      #Request data and decode
-     # data = request.form['user_data']
-     # data = json.loads(data)
+     data = request.form['user_data']
+     data = json.loads(data)
+     print(data['member_id'])
 
-     # user = pd.DataFrame()
+     user = pd.DataFrame()
 
-     # try: 
-     #      user["gender"] = [data['gender']]
-     # except:
-     #      user['gender'] = [np.nan]
-     
-     # try: 
-     #      user["age"] = [data['age']]
-     # except:
-     #      user['age'] = [np.nan]
+     try: 
+          user["gender"] = [data['gender']]
+     except:
+          user['gender'] = [np.nan]
 
-     # try: 
-     #      user["marital_status"] = [data['marital_status']]
-     # except:
-     #      user['marital_status'] = [np.nan]
-     
-     # try: 
-     #      user["ads"] = [data['ads']]
-     # except:
-     #      user['ads'] = [np.nan]
-     
-     # try: 
-     #      user["present_state"] = [data['present_state']]
-     # except:
-     #      user['present_state'] = [np.nan]
+     try: 
+          user["age"] = [data['age']]
+     except:
+          user['age'] = [np.nan]
 
-     # try: 
-     #      user["highest_education"] = [data['highest_education']]
-     # except:
-     #      user['highest_education'] = [np.nan]
+     try: 
+          user["marital_status"] = [data['marital_status']]
+     except:
+          user['marital_status'] = [np.nan]
 
-     # try: 
-     #      user["occupation"] = [data['occupation']]
-     # except:
-     #      user['occupation'] = [np.nan]
+     try: 
+          user["on_behalf"] = [data['on_behalf']]
+     except:
+          user['on_behalf'] = [np.nan]
 
-     # try: 
-     #      user["employed"] = [data['employed']]
-     # except:
-     #      user['employed'] = [np.nan]
+     try: 
+          user["ads"] = [urllib.parse.unquote(data['ads'])]
+     except:
+          user['ads'] = [np.nan]
 
-     # try: 
-     #      user["caste"] = [data['caste']]
-     # except:
-     #      user['caste'] = [np.nan]
+     try: 
+          user["present_state"] = [data['present_state']]
+     except:
+          user['present_state'] = [np.nan]
 
-     # try: 
-     #      user["sect"] = [data['sect']]
-     # except:
-     #      user['sect'] = [np.nan]
+     try: 
+          user["highest_education"] = [data['highest_education']]
+     except:
+          user['highest_education'] = [np.nan]
 
-     # try: 
-     #      user["family_type"] = [data['family_type']]
-     # except:
-     #      user['family_type'] = [np.nan]
+     try: 
+          user["occupation"] = [data['occupation']]
+     except:
+          user['occupation'] = [np.nan]
 
-     # try: 
-     #      user["platform"] = [data['platform']]
-     # except:
-     #      user['platform'] = [np.nan]
+     try: 
+          user["employed"] = [data['employed']]
+     except:
+          user['employed'] = [np.nan]
 
-     # try: 
-     #      user["income_rs"] = [data['income_rs']]
-     # except:
-     #      user['income_rs'] = [np.nan]
+     try: 
+          user["caste"] = [data['caste']]
+     except:
+          user['caste'] = [np.nan]
+
+     try: 
+          user["sect"] = [data['sect']]
+     except:
+          user['sect'] = [np.nan]
+
+     try: 
+          user["family_type"] = [data['family_type']]
+     except:
+          user['family_type'] = [np.nan]
+
+     try: 
+          user["platform"] = [data['platform']]
+     except:
+          user['platform'] = [np.nan]
+
+     try: 
+          user["income_rs"] = [data['income_rs']]
+     except:
+          user['income_rs'] = [np.nan]
 
 
-     user = pd.read_csv('one_user_data.csv',encoding='utf-8')
+     # user = pd.read_csv('one_user_data.csv',encoding='utf-8')
      # print(user.columns)
-
+     # user.to_csv('data.csv',index=False)
+     # user = pd.read_csv('data.csv',dtype=np.float64)
+     
 
 
      #Income_rs
@@ -122,11 +126,12 @@ def test():
                if x.split(' ')[1]=='0':
                     return int(x.split(' ')[3])
                return int(x.split(' ')[1])
-          
+
+     
      user['income_rs'] = income(user['income_rs'][0])
-     
- 
-     
+
+
+
      #Ads
      ads_list = ["gclid", 'organic',"set", 'fb','Funnel','matrimony']
 
@@ -139,7 +144,7 @@ def test():
                     elif item.lower() in word.lower():
                          return item
           return None
-     
+
      try:
         str_res = get_first_word(user['ads'][0],ads_list)
         if(str_res==None):
@@ -156,7 +161,7 @@ def test():
      #States
      other_states = ['Tripura','Manipur','Lakshadweep','Nagaland','Andaman And Nicobar','Arunachal Pradesh','Mizoram',
                  'Dadra and Nagar Haveli','Sikkim','Himachal Pradesh','Meghalaya','Chandigarh','Daman and Diu','Pogradec']
-        
+
      if user['present_state'][0] in other_states:
                user['present_state'] = ['Other_states']
 
@@ -175,16 +180,16 @@ def test():
 
      if user['highest_education'][0] in e1:
                user['highest_education'] = ['Education_category_1']
-    
+
      elif user['highest_education'][0] in e2:
                user['highest_education'] = ['Education_category_2']
-        
+
      elif user['highest_education'][0] in e3:
                user['highest_education'] = ['Education_category_3']
-        
+
      elif user['highest_education'][0] in e4:
                user['highest_education'] = ['Education_category_4']
-        
+
      elif user['highest_education'][0] in e5:
                user['highest_education'] = ['Education_category_5']
 
@@ -193,11 +198,11 @@ def test():
      # print(user['highest_education'][0])
      # print(e4[0])
 
-     
 
 
 
-     
+
+
      #Occupation
 
      o1=['VP/ AVP/ GM/ DGM', 'Research Professional', 'Surgeon', 'Consultant', 'CxO/ Chairman/ Preside', 'Professor/Lecturer', 'Software Professional', 'Scientist', 'Program Manager', 'Research Assistant', 'Subject Matter Expert']
@@ -215,16 +220,16 @@ def test():
 
      if user['occupation'][0] in o1:
           user['occupation'] = ['occupation_category_1']
-    
+
      elif user['occupation'][0] in o2:
           user['occupation'] = ['occupation_category_2']
-        
+
      elif user['occupation'][0] in o3:
           user['occupation'] = ['occupation_category_3']
-        
+
      elif user['occupation'][0] in o4:
           user['occupation'] = ['occupation_category_4']
-        
+
      elif user['occupation'][0] in o5:
           user['occupation'] = ['occupation_category_5']
 
@@ -234,7 +239,7 @@ def test():
      else:
           user['occupation'] = ['occupation_category_7']
 
-     
+
 
 
 
@@ -251,13 +256,13 @@ def test():
 
      if user['caste'][0] in c1:
           user['caste'] = ['caste_category_1']
-    
+
      elif user['caste'][0] in c2:
           user['caste'] = ['caste_category_2']
-        
+
      elif user['caste'][0] in c3:
           user['caste'] = ['caste_category_3']
-        
+
      elif user['caste'][0] in c4:
           user['caste'] = ['caste_category_4']
      else:
@@ -287,7 +292,7 @@ def test():
      # #One Hot Encoding
      # columns_to_encode = ['gender', 'marital_status', 'on_behalf','ads','present_state','highest_education','occupation','employed',
      #                'caste','sect','family_type','platform']
-     
+
      # one_hot_df = pd.read_csv('one_hot_data_premium.csv',encoding='utf-8')
 
      # # print(user)
@@ -295,10 +300,10 @@ def test():
      # # load the encoder object from file
      # with open('encoder.pkl', 'rb') as f:
      #      encoder_model = pickle.load(f)
-     
+
 
      # # print(len(encoder_model.transform(user[columns_to_encode]).toarray()[0]))
-     
+
      # one_hot_df =  one_hot_df.drop(columns=['member_id','age','income_rs','membership'])
      # # print(one_hot_df.columns)
      # one_hot_user = pd.DataFrame()
@@ -306,8 +311,10 @@ def test():
 
      columns_to_encode = ['gender', 'marital_status', 'on_behalf','ads','present_state','highest_education','occupation','employed',
                      'caste','sect','family_type','platform']
-     
+
      one_hot_df = pd.read_csv('one_hot_data_premium_null.csv',encoding='utf-8')
+
+     print(user.columns)
 
      # load the encoder object from file
      with open('encoder.pkl', 'rb') as f:
@@ -324,7 +331,7 @@ def test():
 
      # print(one_hot_df)
 
-     
+
 
 
      lst_age=[]
@@ -348,6 +355,7 @@ def test():
      # print(model.predict_proba(one_hot_df))
 
      return str(model.predict_proba(one_hot_df)[0][1])
+
 
 
 if __name__ == "__main__":
